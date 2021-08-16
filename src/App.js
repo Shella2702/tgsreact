1de16626b85f9b2a3ef5ee377f9111ab34b924c2
Shella Batrisyia L <shella.batrisyia.lupita@gmail.com>
2021-08-16 23:19:26 +0700

    first react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import profile from './img/profile2.png';
+import Thumbs1 from './img/1.png';
+import Thumbs2 from './img/2.png';
+import Thumbs3 from './img/nd003.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <section className ="profile">
+     <div className="header">
+       <img src ={profile} alt ="profile"></img>
+     </div>
+     <div className ="row">
+       <h2>Hai Selamat Datang Kawan, </h2>
+       <p> Nama saya Shella Batrisyia, kamu bisa memanggil saya Naga. saya Dari Sidoarjo</p>
+     </div>
+   </section>
+
+   <section>
+     <div>About</div>
+     <div>
+       <p> saya saya huhuhu</p>
+     </div>
+
+     <div>Hobi</div>
+     <div>
+       <div>
+         <img src ={Thumbs1}></img>
+         <p>Bersepeda.</p>
+       </div>
+     </div>
+
+     <div>
+       <div>
+         <img src = {Thumbs2}></img>
+         <p>Berenang.</p>
+       </div>
+     </div>
+
+     <div>
+       <div>
+         <img src = {Thumbs3}></img>
+         <p>Jelajah Alam</p>
+       </div>
+     </div>
+   </section>
+
+   <section>
+     <h2>Contact Us</h2>
+     <ul>
+       <li>Lokasi : Sidoarjo</li>
+       <li>My Office</li>
+     </ul>
+   </section>
+
+   <footer>Happy Cod</footer>
+</>
   );
 }
 
